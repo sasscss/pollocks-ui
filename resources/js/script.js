6f5585440f2aab2a97b1aa218484986503bc500f
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-06-03 18:54:33 +0530

    changes in 3rd section

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,15 +2,15 @@
 /* .ready - it ensures that can only run after the document has been loaded */
 $(document).ready(function(){
      
-    $('#location--4').mouseover(function(){ 
-        $(this).parent().animate({marginLeft:"-30vw"},500); 
-    });
+    // $('#location--4').mouseover(function(){ 
+    //     $(this).parent().animate({marginLeft:"-23vw"},500); 
+    // }); 
     $('#location--5').mouseover(function(){ 
-        $(this).parent().animate({marginLeft:"-30vw"},500); 
-    });  
-    $('#location--3').mouseover(function(){
-        $(this).parent().animate({marginLeft:"0px"},500);  
-    }); 
+        $(this).parent().animate({marginLeft:"-23vw"},500);  
+     });  
+    // $('#location--3').mouseover(function(){
+    //     $(this).parent().animate({marginLeft:"0px"},500);  
+    // }); 
     $('#location--2').mouseover(function(){
         $(this).parent().animate({marginLeft:"0px"},500);  
     });
