c4db5a5eb0aa86127441da1d3c7ea102ae98e700
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-06-13 00:55:22 +0530

    animation change

--- resources/js/script.js
+++ resources/js/script.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
     $('.js--wp-2-2').waypoint(function(direction){
         $('.js--wp-2-2').addClass('animate__animated animate__fadeIn');
     },{    
-        offset:"95%"             
+        offset:"90%"             
     }); 
 
     // js--wp-3-1 3rd sec 1st component        
     $('.js--wp-3-1').waypoint(function(direction){
         $('.js--wp-3-1').addClass('animate__animated animate__fadeIn');
     },{  
-        offset:"70%"    
+        offset:"65%"    
     }); 
 
     // js--wp-3-2-1 3rd sec 2nd component  1st item      
@@ -53,7 +53,7 @@ $(document).ready(function(){
     $('.js--wp-3-2-1').waypoint(function(direction){
         $('.js--wp-3-2-1').addClass('animate__animated card-fade-in-right');
     },{   
-        offset:"85%"     
+        offset:"70%"     
     }); 
 
     // js--wp-3-2-2 3rd sec 2nd component  2nd item      
@@ -61,7 +61,7 @@ $(document).ready(function(){
     $('.js--wp-3-2-2').waypoint(function(direction){
         $('.js--wp-3-2-2').addClass('animate__animated card-fade-in-left');
     },{   
-        offset:"85%"    
+        offset:"70%"    
     }); 
 
     $('.counter').counterUp({ 
