8056b20e81990b33cdf7ee74a050e1dbf93a616b
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-06-13 00:22:38 +0530

    aniations included

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,74 @@
 
 /* .ready - it ensures that can only run after the document has been loaded */
 $(document).ready(function(){
-     
+
+    //js--wp-1 1st section after header
+
+    $('.js--wp-1-1').waypoint(function(direction){
+        $('.js--wp-1-1').addClass('animate__animated animate__fadeIn');
+    },{ 
+        offset:"70%"      
+    });   
+
+    $('.js--wp-1-2-1').waypoint(function(direction){
+        $('.js--wp-1-2-1').addClass('animate__animated card-fade-in-right');
+    },{ 
+        offset:"75%"      
+    }); 
+    $('.js--wp-1-2-2').waypoint(function(direction){
+        $('.js--wp-1-2-2').addClass('animate__animated card-fade-in-left');
+    },{ 
+        offset:"75%"      
+    }); 
+    $('.js--wp-1-2-3').waypoint(function(direction){
+        $('.js--wp-1-2-3').addClass('animate__animated card-fade-in-up');
+    },{ 
+        offset:"75%"      
+    });    
+
+
+    // js--wp-2-1 2nd sec 1st component
+    $('.js--wp-2-1').waypoint(function(direction){
+        $('.js--wp-2-1').addClass('animate__animated animate__fadeInRight');
+    },{  
+        offset:"75%"      
+    }); 
+    
+    // js--wp-2-1 2nd sec 2nd component    
+    $('.js--wp-2-2').waypoint(function(direction){
+        $('.js--wp-2-2').addClass('animate__animated animate__fadeIn');
+    },{    
+        offset:"90%"            
+    }); 
+
+    // js--wp-3-1 3rd sec 1st component        
+    $('.js--wp-3-1').waypoint(function(direction){
+        $('.js--wp-3-1').addClass('animate__animated animate__fadeIn');
+    },{  
+        offset:"70%"    
+    }); 
+
+    // js--wp-3-2-1 3rd sec 2nd component  1st item      
+
+    $('.js--wp-3-2-1').waypoint(function(direction){
+        $('.js--wp-3-2-1').addClass('animate__animated card-fade-in-right');
+    },{   
+        offset:"85%"     
+    }); 
+
+    // js--wp-3-2-2 3rd sec 2nd component  2nd item      
+
+    $('.js--wp-3-2-2').waypoint(function(direction){
+        $('.js--wp-3-2-2').addClass('animate__animated card-fade-in-left');
+    },{   
+        offset:"85%"    
+    }); 
+
+    $('.counter').counterUp({ 
+        delay: 10,  
+        time: 2000             
+    });
+
     // $('#location--4').mouseover(function(){ 
     //     $(this).parent().animate({marginLeft:"-19vw"},500); 
     // }); 
