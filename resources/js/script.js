9d7c151f36da1a6830c176c4b656dfccf27385dc
aravindSiruvuru05 <siruvuruaravind@gmail.com>
2020-06-17 17:22:00 +0530

    sdljfh

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,7 +60,7 @@ document.onreadystatechange = function () {
       $('.js--wp-3-1').waypoint(function(direction){
           $('.js--wp-3-1').addClass('animate__animated card-fade-in-up'); 
       },{   
-          offset:"65%"      
+          offset:"60%"      
       }); 
   
       // js--wp-3-2-1 3rd sec 2nd component  1st item      
